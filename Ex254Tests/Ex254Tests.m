7435ff67841c4ef01ce967e94700ad5854b16e23
saerem21 <nemessisdds>
2014-01-08 12:25:57 +0900

    Initial Commit

---
+++ Ex254Tests/Ex254Tests.m
@@ -0,0 +1,34 @@
+//
+//  Ex254Tests.m
+//  Ex254Tests
+//
+//  Created by SDT-1 on 2014. 1. 8..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex254Tests : XCTestCase
+
+@end
+
+@implementation Ex254Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
