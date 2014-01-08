1e12fec1e26475a63e681cdaf4f864f884eb7feb
saerem21 <nemessisdds>
2014-01-08 12:44:33 +0900

    페이지 컨트롤러 예제입니다..
    254page

--- Ex254/ViewController.m
+++ Ex254/ViewController.m
@@ -7,17 +7,68 @@
 //
 
 #import "ViewController.h"
+#define IMAGE_NUM 4
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UIScrollView *_scrollView;
+    UIPageControl *_pageControl;
+    int _loadedPageCount;
+}
 
+- (void)loadContentsPage:(int)pageNo {
+    if (pageNo < 0 || pageNo < _loadedPageCount || pageNo >= IMAGE_NUM) {
+        return;
+    }
+    float width = _scrollView.frame.size.width;
+    float height = _scrollView.frame.size.height;
+    
+    NSString *fileName = [NSString stringWithFormat:@"i%d", pageNo];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:fileName ofType:@"jpeg"];
+    UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    
+    imageView.frame = CGRectMake(width * pageNo, 0, width, height);
+    [_scrollView addSubview:imageView];
+    _loadedPageCount++;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    float width = scrollView.frame.size.width;
+    float offsetX = scrollView.contentOffset.x;
+    int pageNo = floor(offsetX / width);
+    
+    _pageControl.currentPage = pageNo;
+    
+    [self loadContentsPage:pageNo-1];
+    [self loadContentsPage:pageNo];
+    [self loadContentsPage:pageNo+1];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_scrollView];
+    
+    float width = _scrollView.bounds.size.width;
+    float height = _scrollView.bounds.size.height;
+    
+    _scrollView.delegate = self;
+    _scrollView.pagingEnabled = YES;
+    _scrollView.contentSize = CGSizeMake(width * IMAGE_NUM, height);
+    
+    _pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(130, 400, 60, 40)];
+    [self.view addSubview:_pageControl];
+    _pageControl.numberOfPages = IMAGE_NUM;
+    
+    _loadedPageCount = 0;
+    [self loadContentsPage:0];
+    [self loadContentsPage:1];
+    
 }
 
 - (void)didReceiveMemoryWarning
